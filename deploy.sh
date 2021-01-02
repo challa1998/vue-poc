f7963dcda800147a82c73fcd743fadc54f4bdd34
rithik <19rithik@gmail.com>
2021-01-02 16:00:06 +0530

    Modified deploy file

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:challa1998/challa1998.github.io.git master
+#git push -f git@github.com:challa1998/challa1998.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:challa1998/vue-poc.git master:gh-pages
